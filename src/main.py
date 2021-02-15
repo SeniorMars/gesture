2d37f2ae6524c8f475d93bb708b593c378bc5114
AmitPr <17amitprasad@gmail.com>
2021-02-14 20:59:56 -0500

    Start working on background subtraction

--- src/main.py
+++ src/main.py
@@ -3,13 +3,15 @@ import cv2
 
 #Open webcam stream
 stream = cv2.VideoCapture(0)
-
+backSub = cv2.createBackgroundSubtractorKNN()
 while(True):
     # Capture frame from stream
     ret, frame = stream.read()
+    mask = backSub.apply(frame)
+    cv2.dilate(mask,(3,3))
 
     # Show frame
-    cv2.imshow('frame',frame)
+    cv2.imshow('frame',mask)
     #Quit if 'q' is pressed
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
