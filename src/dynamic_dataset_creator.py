54235c98a3dec512e69074f6394536e9d0724314
AmitPr <17amitprasad@gmail.com>
2021-03-21 11:41:06 -0400

    Create seperate dataset creators for static and dynamic gestures.

--- src/dynamic_dataset_creator.py
+++ src/dynamic_dataset_creator.py
@@ -1,47 +1,46 @@
+from PIL import Image, ImageTk
+import tkinter as tk
+import h5py
 import numpy as np
 from cv2 import cv2
 import mediapipe as mp
 from os.path import exists
-import tensorflow as tf
-physical_devices = tf.config.list_physical_devices('GPU') 
-if len(physical_devices) > 0:
-    tf.config.experimental.set_memory_growth(physical_devices[0], True)
-import h5py
 
-import tkinter as tk
-from PIL import Image, ImageTk
 
-filename = 'dgestures.hdf5'
+filename = 'dynamic_gestures.hdf5'
 if not exists(filename):
     f = h5py.File(filename, 'x')
 else:
     f = h5py.File(filename, 'r+')
 current_dataset = None
 
-model = tf.keras.models.load_model('test_model_1')
-model_labels = ["five_finger", "four_finger", "ok",
-                "one_finger", "three_finger", "two_finger"]
+# How long a gesture is in frames.
+gestureLength = 20
+framesRemaining = gestureLength
+gestureAnchor = (0, 0, 0)
 
 
 def toggleRecording():
-    global recording, startStopButton, current_dataset, framesRemaining
+    global recording, startStopButton, current_dataset, framesRemaining, gestureLength
     recording = not recording
-    framesRemaining = 20
+    framesRemaining = gestureLength
     if recording:
         startStopButton.config(text="Stop Recording")
         # Open up the corresponding data set for the gesture type
         gestureType = gestureNameEntry.get()
-        if "/"+gestureType+"/ds1" not in f:
+        if "/"+gestureType not in f:
             current_dataset = f.create_dataset(
-                '/'+gestureType+'/ds1', (0, 21, 3), maxshape=(None, 21, 3), dtype='float64')
+                '/'+gestureType, (0, gestureLength, 21, 3), maxshape=(None, gestureLength, 21, 3), dtype='float64')
         else:
-            current_dataset = f[gestureType]['ds1']
+            current_dataset = f[gestureType]
+            current_dataset.resize(
+                (current_dataset.shape[0]+1, current_dataset.shape[1], current_dataset.shape[2], current_dataset.shape[3]))
     else:
         startStopButton.config(text="Start Recording")
 
 
 def show_frame():
-    global cap, current_dataset, predictionString, framesRemaining
+    global cap, current_dataset, framesRemaining, gestureLength, gestureAnchor
     success, image = cap.read()
     if not success:
         print("Ignoring empty camera frame.")
@@ -56,26 +55,28 @@ def show_frame():
     results = hands.process(image)
     # Draw the hand annotations on the image.
     image.flags.writeable = True
-    
+
     image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
     if results.multi_hand_landmarks:
-        wrist = results.multi_hand_landmarks[0].landmark[mp_hands.HandLandmark.WRIST]
-        wrist = (wrist.x, wrist.y, wrist.z)
+        # Get the wrist point of the hand to use as an anchor.
+        if framesRemaining == gestureLength:
+            wrist = results.multi_hand_landmarks[0].landmark[mp_hands.HandLandmark.WRIST]
+            gestureAnchor = (wrist.x, wrist.y, wrist.z)
         hand = []
         for hand_landmarks in results.multi_hand_landmarks:
             for i in hand_landmarks.ListFields()[0][1]:
-                hand.append(tuple(np.subtract((i.x, i.y, i.z), wrist)))
+                # Add each point to the hand array, normalized relative to the anchor.
+                hand.append(tuple(np.subtract((i.x, i.y, i.z), gestureAnchor)))
             mp_drawing.draw_landmarks(
                 image, hand_landmarks, mp_hands.HAND_CONNECTIONS)
         if recording:
-            if isChecked.get() and framesRemaining<=0: toggleRecording()
+            if framesRemaining <= 0:
+                toggleRecording()
             framesRemaining -= 1
-            # Add Data to H5PY file then extend its size
-            current_dataset.resize(
-                (current_dataset.shape[0]+1, current_dataset.shape[1], current_dataset.shape[2]))
-            current_dataset[current_dataset.shape[0]-1, :, :] = np.array(hand)
-        prediction = model.predict(np.array(hand)[None, :])
-        predictionString.set("Current Guess: " + model_labels[np.argmax(prediction)] + " " + str(np.round(np.max(prediction)*100, decimals=2))+"%")
+            # Add our data to h5 file.
+            current_dataset[current_dataset.shape[0]-1,
+                            gestureLength-framesRemaining, :, :] = np.array(hand)
+
     img = Image.fromarray(cv2.cvtColor(image, cv2.COLOR_BGR2RGBA))
     imgtk = ImageTk.PhotoImage(image=img)
     lmain.imgtk = imgtk
@@ -106,16 +107,6 @@ gestureNameEntry.grid(row=0, column=1, padx=10)
 startStopButton = tk.Button(
     ctrlPanel, text="Start Recording", command=toggleRecording)
 startStopButton.grid(row=0, column=2, padx=10)
-isChecked = tk.BooleanVar()
-framesRemaining = 20
-isOverTime = tk.Checkbutton(
-    ctrlPanel, text='Dynamic?', variable=isChecked, onvalue=True, offvalue=False)
-isOverTime.grid(row=0, column=3, padx=10)
-
-predictionString = tk.StringVar()
-predictionString.set("No Guess.")
-currentLabel = tk.Label(ctrlPanel, textvariable=predictionString)
-currentLabel.grid(row=0, column=4)
 
 # Setup MediaPipe Hands
 mp_drawing = mp.solutions.drawing_utils
@@ -124,6 +115,7 @@ hands = mp_hands.Hands(
     min_detection_confidence=0.5, min_tracking_confidence=0.75, max_num_hands=1
 )
 
+# OpenCV video stream at 20 fps.
 cap = cv2.VideoCapture(0)
 cap.set(cv2.CAP_PROP_FPS, 20)
 show_frame()
